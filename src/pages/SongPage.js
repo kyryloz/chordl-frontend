64db88d05f3214168647562299170986f43387dc
zak <zak@swingpulse.com>
2016-08-09 18:51:52 +0300

    Impl popover menu

--- src/pages/SongPage.js
+++ src/pages/SongPage.js
@@ -3,24 +3,35 @@ import * as $ from "jquery";
 import FlatButton from 'material-ui/FlatButton';
 import {Link, hashHistory} from 'react-router';
 import colors from "../colors";
+import PopoverItemMenu from "../components/PopoverItemMenu";
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Divider from 'material-ui/Divider';
 
 const urlSong = 'http://localhost:8081/api/songs/';
 
 const styles = {
     page: {
-        marginLeft: '70px',
-        marginRight: '70px',
         display: 'flex',
         flexDirection: 'column'
     },
 
     pageTitle: {
+        marginLeft: 70,
+        marginRight: 16,
         display: 'flex',
         flexDirection: 'row'
     },
 
     title: {
         flexGrow: 1
+    },
+
+    content: {
+        marginLeft: 70,
+        marginRight: 70
     },
 
     link: {
@@ -42,7 +53,8 @@ export default class SongPage extends React.Component {
                 performerName: "",
                 title: "",
                 lyrics: ""
-            }
+            },
+            contextMenuOpened: false
         }
     }
 
@@ -68,7 +80,9 @@ export default class SongPage extends React.Component {
         });
     }
 
-    handleDelete = () => {
+    handleDelete = (e) => {
+        e.preventDefault();
+
         $.ajax({
             url: this.urlGetSong,
             type: 'DELETE',
@@ -83,6 +97,11 @@ export default class SongPage extends React.Component {
                 console.error(status, err);
             }
         });
+    };
+
+    handleEdit = (e) => {
+        e.preventDefault();
+        hashHistory.push("song/" + this.state.song.id + "/edit");
     };
 
     render() {
@@ -106,20 +125,23 @@ export default class SongPage extends React.Component {
                             {this.state.song.title}
                         </h3>
                     </div>
-                    <div style={{marginTop: 10}}>
-                        <FlatButton
-                            label="Edit"
-                            primary={true}
-                            href={"#/song/" + this.state.song.id + "/edit"}
-                        />
-                        <FlatButton
-                            label="Delete"
-                            labelStyle={{color: 'red'}}
-                            onTouchTap={this.handleDelete}
-                        />
+                    <div style={{marginTop: 6}}>
+                        <IconMenu
+                            iconButtonElement={<IconButton>
+                                <MoreVertIcon color={colors.defaultPrimaryColor}/>
+                            </IconButton>}
+                            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                        >
+                            <MenuItem primaryText="Edit" onTouchTap={this.handleEdit}/>
+                            <MenuItem style={{color: "red"}} primaryText="Delete" onTouchTap={this.handleDelete}/>
+                        </IconMenu>
                     </div>
                 </div>
-                <pre>{this.state.song.lyrics}</pre>
+                <Divider />
+                <div style={styles.content}>
+                    <pre>{this.state.song.lyrics}</pre>
+                </div>
             </div>
         )
     }
