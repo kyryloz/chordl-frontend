240a0744b38f3c87503b037292938ae44e1c1110
zak <zak@swingpulse.com>
2016-09-06 21:00:57 +0300

    Responsive container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,20 +1,38 @@
 import * as React from "react";
 import Header from "./Header";
 
-const styles = {
-    pageContent: {
-        width: "70%",
-        margin: "16px auto",
-    }
-};
-
 export default class MainContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            windowWidth: window.innerWidth
+        }
+    }
+
+    handleResize = () => {
+        console.log(window.innerWidth);
+        this.setState({windowWidth: window.innerWidth});
+    };
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
 
     render() {
         return (
-            <div style={styles.main}>
+            <div>
                 <Header {...this.props}/>
-                <div style={styles.pageContent}>
+                <div style={{
+                    width: this.state.windowWidth > 700 ? "70%" : "95%",
+                    maxWidth: 900,
+                    margin: "16px auto",
+                }}>
                     {React.cloneElement(this.props.children, this.props)}
                 </div>
             </div>
