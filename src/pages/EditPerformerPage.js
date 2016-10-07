31d7f796cec1657fcd2455946c5aebe9d920275b
zak <zak@robotnec.com>
2016-10-07 13:04:54 +0300

    Remove some dependencies

--- src/pages/EditPerformerPage.js
+++ src/pages/EditPerformerPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import update from "react-addons-update";
 import BasePageTemplate from "./BasePageTemplate";
 import {requestGetPerformerById, requestUpdatePerformer, requestDeletePerformer} from "../global/api";
 import {Button, FormGroup, ControlLabel, FormControl} from "react-bootstrap/lib";
@@ -10,10 +9,10 @@ export default class EditPerformerPage extends BasePageTemplate {
     constructor(props) {
         super(props);
 
-        this.state = {performer: {
+        this.state = {
             name: "",
             id: -1
-        }}
+        }
     }
 
     componentDidMount() {
@@ -24,28 +23,26 @@ export default class EditPerformerPage extends BasePageTemplate {
         this.startLoading();
         requestGetPerformerById(this.props.params.id)
             .then(data => {
-                this.setState({performer: data});
+                this.setState({...data});
                 this.finishLoading();
             })
             .catch(console.error);
     }
 
     updatePerformer() {
-        requestUpdatePerformer(this.state.performer)
+        const {name, id} = this.state;
+        requestUpdatePerformer({name, id})
             .then(data => {
-                this.setState({performer: data});
+                this.setState({...data});
                 this.handleCancel();
             })
             .catch(console.error);
     }
 
     handleNameChange = (event) => {
-        const newState = update(this.state, {
-            performer: {
-                name: {$set: event.target.value}
-            }
+        this.setState({
+            name: event.target.value
         });
-        this.setState(newState);
     };
 
     handleSave = (e) => {
@@ -54,11 +51,11 @@ export default class EditPerformerPage extends BasePageTemplate {
     };
 
     handleCancel = () => {
-        this.context.router.replace("/performer/" + this.state.performer.id)
+        this.context.router.replace("/performer/" + this.state.id)
     };
 
     deletePerformer() {
-        requestDeletePerformer(this.state.performer.id)
+        requestDeletePerformer(this.state.id)
             .then(() => {
                 this.context.router.replace("/");
             })
@@ -72,7 +69,7 @@ export default class EditPerformerPage extends BasePageTemplate {
     renderHeader() {
         return (
             <h3>
-                {this.state.performer.name}
+                {this.state.name}
             </h3>
         )
     }
@@ -82,21 +79,21 @@ export default class EditPerformerPage extends BasePageTemplate {
             <form onSubmit={this.handleSave}>
                 <FormGroup
                     controlId="formBasicText"
-                    validationState={validator.validatePerformer(this.state.performer.name, true)}
+                    validationState={validator.validatePerformer(this.state.name, true)}
                 >
                     <ControlLabel>Name</ControlLabel>
                     <FormControl
                         style={{fontFamily: "monospace"}}
                         type="text"
                         placeholder="Name"
-                        value={this.state.performer.name}
+                        value={this.state.name}
                         onChange={this.handleNameChange}
                     />
                     <FormControl.Feedback />
                 </FormGroup>
                 <FormGroup>
                     <Button
-                        disabled={this.state.performer.name.length === 0}
+                        disabled={this.state.name.length === 0}
                         type="submit"
                         bsStyle="success"
                         style={{width: 120}}>
